13f28a6a82c6ddf3500015d0d18cfe3c4ec11598
Surwxa5 <nepalisurakshya767@gmail.com>
2026-05-28 15:48:08 +0545

    recipe detail

--- src/app/recipes/page.js
+++ src/app/recipes/page.js
@@ -1,20 +1,22 @@
+import Link from 'next/link'
 export default async function Recipes(){
   const res = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
   const data= await res.json()
 
-//  i will work on this tomorrow
+
   return(
     <div style={{maxWidth:"900px", margin:"0 auto", padding:'20px'}}>
       <h1>Recipes</h1>
-      <div style={{display:"grid",gridTemplateColumns:"repeat(auto-fill,minmax(200px,1fr))",gap:"16px"}}>
+    <div style={{display:"grid",gridTemplateColumns:"repeat(auto-fill,minmax(200px,1fr))",gap:"16px"}}>
    {data.meals.map((meal)=>(
+    <Link href={`/recipes/${meal.idMeal}`}>
     <div key={meal.idMeal} style={{border:'1px solid #ddd',borderRadius:'8px',overflow:"hidden"}}>
-      <img src={meal.strMealThumb} alt={meal.strMeal} style={{width:"100%",height:"180px",objectFit:"cover"}}/>
+       <img src={meal.strMealThumb} alt={meal.strMeal} style={{width:"100%",height:"180px",objectFit:"cover"}}/>
       <div style={{padding:"12px"}}>
-        <h3 style={{padding:"12px"}}>{meal.strMeal}</h3>
+      <h3 style={{padding:"12px"}}>{meal.strMeal}</h3> 
       </div>
     </div>
-   
+    </Link >
    ))}
     </div>
      </div>
